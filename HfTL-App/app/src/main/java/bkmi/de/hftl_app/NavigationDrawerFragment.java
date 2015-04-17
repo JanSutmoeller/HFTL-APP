f98779c7b7d7778db9049b1b06fc32dff264fbda
geoebe <geoebe@users.noreply.github.com>
2015-04-17 18:46:19 +0200

    News, Optionsmenü, Logo hinzugefügt

--- HfTL-App/app/src/main/java/bkmi/de/hftl_app/NavigationDrawerFragment.java
+++ HfTL-App/app/src/main/java/bkmi/de/hftl_app/NavigationDrawerFragment.java
@@ -1,5 +1,6 @@
 package bkmi.de.hftl_app;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.app.Activity;
 import android.support.v7.app.ActionBar;
@@ -261,6 +262,11 @@ public class NavigationDrawerFragment extends Fragment {
 
         if (item.getItemId() == R.id.action_example) {
             Toast.makeText(getActivity(), "BKMI13", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+
+        if (item.getItemId() == R.id.action_settings) {
+            startActivity(new Intent(getActivity(), EinstellungActivity.class));
             return true;
         }
 
